75181ef7d2795dca1b91ff61e915728cc1fe194c
Paul Franco <mail@paulfran.co>
2021-03-12 23:08:05 -0800

    Initial Commit

---
+++ SwiftUI Audio Recorder/SwiftUI_Audio_RecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Audio_RecorderApp.swift
+//  SwiftUI Audio Recorder
+//
+//  Created by Paul Franco on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Audio_RecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
